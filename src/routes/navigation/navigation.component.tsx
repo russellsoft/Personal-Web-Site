408021159cafc6fbbca18a3abff94cf2139022c6
Ruslan <105549663+russellsoft@users.noreply.github.com>
2023-02-19 21:19:47 +0300

    Update navigation.component.tsx

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import { NavigationContainer } from "./navigation.styles"
 
 const Navigation = () => {
 	return (
-		<div className="nav"></div>
+		<NavigationContainer>
+			<h2>Ruslan Makiev</h2>
+			<h2>Главная</h2>
+		</NavigationContainer>
 	)
 }
 
