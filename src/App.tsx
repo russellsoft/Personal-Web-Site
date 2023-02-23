fcc5f636d9195a7b0fc1dae6b7177cd07bb61534
Руслан Макиев <ruslanmakiev@Ruslans-MacBook-Air.local>
2023-02-23 20:26:33 +0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Home from './routes/home/home.component'
 import GlobalStyle from "./global.styles";
 import Spinner from "./components/spinner/spinner.component";
-import Footer from "./components/footer/footer.component";
+import Footer from "./components/footer/footer.component"
 
 export const App = () => {
 	return (
